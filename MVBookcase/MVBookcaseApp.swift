35cf225dfcebccdf592651ff4e46f6ea351db408
Jonas Agnezi <jonas.agnezi@gmail.com>
2022-08-28 16:01:21 -0300

    Initial Commit

---
+++ MVBookcase/MVBookcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVBookcaseApp.swift
+//  MVBookcase
+//
+//  Created by Jonas Agnezi on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MVBookcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
